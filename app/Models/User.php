bfddb21e83233519cb7529ea8349b4388e4c6536
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2025-09-24 11:01:55 +0800

    Initial Commit

---
+++ app/Models/User.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+        'personal_info',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'personal_info' => 'array',
+        ];
+    }
+
+    /**
+     * Get the kit orders for the user.
+     */
+    public function kitOrders()
+    {
+        return $this->hasMany(KitOrder::class);
+    }
+
+    /**
+     * Get the consultation requests for the user.
+     */
+    public function consultationRequests()
+    {
+        return $this->hasMany(ConsultationRequest::class);
+    }
+
+    /**
+     * Check if user has admin role.
+     */
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
+
+    /**
+     * Check if user has client role.
+     */
+    public function isClient(): bool
+    {
+        return $this->role === 'client';
+    }
+}
